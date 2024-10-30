76065c17393be875160022cf16d700af67f0d592
axelyadieldiaz <alexddiax@gamil.com>
2024-10-30 12:05:51 -0500

    feat:solucion del ejercicio 1

---
+++ 1.contraseña_valida/ejerciccio.js
@@ -0,0 +1,9 @@
+function contrasenaValida(password) {
+    return password === "2Fj(jjbFsuj" || password === "eoZiugBf&g9";
+}
+
+// código de prueba
+console.log(contrasenaValida("2Fj(jjbFsuj")) // true
+console.log(contrasenaValida("eoZiugBf&g9")) // true
+console.log(contrasenaValida("hola")) // false
+console.log(contrasenaValida("")) // false
