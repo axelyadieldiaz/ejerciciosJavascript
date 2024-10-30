276d78092f2f1e2baa8da76d01988c0d524dcfae
axelyadieldiaz <alexddiax@gamil.com>
2024-10-30 12:29:41 -0500

    feat:solucion del ejercicio 4

---
+++ 4. Imprimir_un_arreglo/ejercicio4.js
@@ -0,0 +1,12 @@
+function imprimirArreglo(...elementos) {
+    for (let elemento of elementos) {
+        console.log(elemento);
+    }
+}
+
+// código de prueba
+imprimirArreglo(1, "Hola", 2, "Mundo");
+// 1
+// Hola
+// 2
+// Mundo
